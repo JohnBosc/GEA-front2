535ace52ca36d3f99d46e520720d9d5286791cf5
Jean Bosco MENGUE <jnrmengue@gmail.com>
2019-01-15 09:56:25 +0100

    connection page and dashboard OK

---
+++ src/app/app.routing.ts
@@ -0,0 +1,32 @@
+import { Routes } from '@angular/router';
+import { AdminLayoutComponent } from './layouts/admin/admin-layout.component';
+import { AuthLayoutComponent } from './layouts/auth/auth-layout.component';
+
+export const AppRoutes: Routes = [{
+  path: '',
+  component: AuthLayoutComponent,
+  children: [{
+    path: '',
+    loadChildren: './authentication/authentication.module#AuthenticationModule'
+  }, {
+    path: 'error',
+    loadChildren: './error/error.module#ErrorModule'
+  }]
+}, {
+  path: '',
+  component: AdminLayoutComponent,
+  children: [{
+    path: 'dashboard',
+    loadChildren: './dashboard/dashboard.module#DashboardModule'
+  }, {
+    path: 'icons',
+    loadChildren: './icons/icons.module#IconsModule'
+  }, {
+    path: 'docs',
+    loadChildren: './docs/docs.module#DocsModule'
+  }]
+}, {
+  path: '**',
+  redirectTo: 'error/404'
+}];
+
---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,76 @@
+import { Component } from '@angular/core';
+import * as shape from 'd3-shape';
+import {
+  single,
+  generateData
+} from '../shared/chartData';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+
+export class DashboardComponent {
+  single: any[];
+  graph: {
+    links: any[],
+    nodes: any[]
+  };
+  dateData: any[];
+  // options
+  showXAxis = true;
+  showYAxis = true;
+  gradient = false;
+  showLegend = false;
+  showXAxisLabel = false;
+  tooltipDisabled = false;
+  xAxisLabel = 'Country';
+  showYAxisLabel = false;
+  yAxisLabel = 'GDP Per Capita';
+  showGridLines = true;
+  roundDomains = false;
+  colorScheme = {
+    domain: [
+    '#0088dd', '#0088ee', '#cccccc', '#000eee', '#ccceee', '#0099ee'
+    ]
+  };
+  schemeType = 'ordinal';
+  // line interpolation
+  curve = shape.curveLinear;
+  // line, area
+  timeline = false;
+  // margin
+  margin = false;
+  marginTop = 40;
+  marginRight = 40;
+  marginBottom = 40;
+  marginLeft = 40;
+  // gauge
+  gaugeMin = 0;
+  gaugeMax = 50;
+  gaugeLargeSegments = 10;
+  gaugeSmallSegments = 5;
+  gaugeTextValue = '';
+  gaugeUnits = 'alerts';
+  gaugeAngleSpan = 240;
+  gaugeStartAngle = -120;
+  gaugeShowAxis = true;
+  gaugeValue = 50; // linear gauge value
+  gaugePreviousValue = 70;
+
+  constructor() {
+    Object.assign(this, {
+      single
+    });
+    this.dateData = generateData(5, false);
+  }
+
+  select(data) {
+    console.log('Item clicked', data);
+  }
+
+  onLegendLabelClick(entry) {
+    console.log('Legend clicked', entry);
+  }
+}
